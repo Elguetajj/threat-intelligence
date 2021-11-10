714ec1be0653ae4169033cf8e6651653687b071b
elguetajj <elguetaj@ufm.edu>
2021-11-09 21:38:53 -0600

    adds fields to apps table

--- db/init/create_db_tables.sql
+++ db/init/create_db_tables.sql
@@ -3,7 +3,9 @@ USE configs;
 
 CREATE TABLE apps (
 	id INTEGER NOT NULL AUTO_INCREMENT, 
-	`name` VARCHAR(100), 
+	`name` VARCHAR(100),
+	`os_cpe` VARCHAR(100) ,
+	`server_cpe` VARCHAR(100),
     created TIMESTAMP,
 	PRIMARY KEY (id)
 );
